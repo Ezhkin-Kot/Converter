e9cfe0fb91580a031296f6193f683eee89a5a728
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2025-01-25 11:00:21 +0400

    feat(authentication.js): rewrite to work with new API

--- Converter-Client/scripts/authentication.js
+++ Converter-Client/scripts/authentication.js
@@ -24,34 +24,39 @@ document.getElementById('auth-form').addEventListener('submit', async function (
             },
             body: JSON.stringify(data),
         });
-        json = await response.json();
-        console.log(json);
 
-        if (json.value.success) {
+        if (response.ok) {
+            json = await response.json();
+            console.log(json);
+
+            // Saving user data
             sessionStorage.setItem("user", JSON.stringify({
                 id: json.value.user.id,
                 login: json.value.user.login,
                 premium: json.value.user.premium,
             }));
             console.log(sessionStorage.getItem("user"));
+
             successMessage.style.color = 'green';
             successMessage.style.opacity = '1';
             setTimeout(() => {
                     window.location.href = 'jpg-png.html';
-                },
-                1000
-            );
-        }
-        else if (!json.value.success) {
+                }, 1000);
+        } else if (response.status === 401) {
+            const errorJson = await response.json();
+            console.error(errorJson.message);
+
             errorMessage.style.opacity = '1';
             setTimeout(() => {
-                    errorMessage.style.opacity = '0';
-                },
-                2000
-            );
+                errorMessage.style.opacity = '0';
+                }, 2000);
+        } else if (response.status === 400) {
+            const errorJson = await response.json();
+            console.error('Bad request:', errorJson.message);
+        } else {
+            console.error('Unexpected response:', response.status, await response.json());
         }
-    }
-    catch (error) {
-        console.log(error);
+    } catch (error) {
+        console.log('Authentication request error:', error);
     }
 });
